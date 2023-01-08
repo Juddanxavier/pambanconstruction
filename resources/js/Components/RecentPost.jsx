6db18ea2efa22fa75892fc091b39346fa1c70179
Juddan <juddan2008@gmail.com>
2023-01-08 05:30:19 +0530

    img

--- resources/js/Components/RecentPost.jsx
+++ resources/js/Components/RecentPost.jsx
@@ -2,7 +2,7 @@ import { Link, usePage } from '@inertiajs/inertia-react';
 import React from 'react'
 
 export default function RecentPost(projects) {
-    const pathUrl = 'https://pambanconstructions.com/'
+    const pathUrl = import.meta.env.VITE_STORAGE_IMG;
     var projects = usePage().props.recentProjects;
 
     return (
