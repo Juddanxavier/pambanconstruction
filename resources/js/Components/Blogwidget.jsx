acab3f3f29996858b4af9482cac05e947f97bc65
Juddan <juddan2008@gmail.com>
2023-01-11 23:27:09 +0530

    link

--- resources/js/Components/Blogwidget.jsx
+++ resources/js/Components/Blogwidget.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import HtmlParser from 'react-html-parser';
 
 export default function Blogwidget() {
-        const pathUrl = import.meta.env.VITE_STORAGE_IMG
+        const pathUrl = 'http://pambanconstructions.com/'
 
     const [blog, setBlog] = useState([])
   const getData = async () => {
