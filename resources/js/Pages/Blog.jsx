7ecbb0ca1832b12be637e03a654435e5edcba61e
Juddan <juddan2008@gmail.com>
2023-01-12 00:03:45 +0530

    blog

--- resources/js/Pages/Blog.jsx
+++ resources/js/Pages/Blog.jsx
@@ -6,7 +6,7 @@ import React, { useState } from 'react'
 import HtmlParser from 'react-html-parser';
 
 export default function Blog() {
-    const pathUrl = 'https://pambanconstructions.com/'
+    const pathUrl = 'https://pambanconstructions.com/storage/'
     const data = usePage().props
     const date = format(new Date(data.blog.created_at), "EEEE, MMMM d, yyyy")
     console.log(data)
@@ -14,14 +14,14 @@ export default function Blog() {
     <Frontendlayout>
           <Head title={`${data.blog.title} - Pamban Construction`} />
           <div className="flex flex-col text-3xl my-10">
-              <h1 className="mx-auto capitalize mx-10 md:mx-20 font-bold text-slate-900">{data.blog.title}</h1>
+              <h1 className="mx-auto capitalize mx-10 md:mx-20 justify-center font-bold text-slate-900">{data.blog.title}</h1>
               <div className="flex justify-center text-sm font-bold text-gray-500 mt-5">
                   {/* <UserCircleIcon className="w-6 mx-2" /> | */}
                   <ClockIcon className="w-6 mx-2" />{date}
               </div>
           </div>
           <div className="md:mx-20 h-96">
-              <img className="object-cover w-full h-full md:rounded-lg" src={pathUrl + `storage/${data.blog.image}`} alt={data.blog.image} />
+              <img className="object-cover w-full h-full md:rounded-lg" src={pathUrl + `${data.blog.image}`} alt={data.blog.image} />
           </div>
           <div className="flex grid m-10 md:m-20 justify-center">
               <div className="max-w-5xl text-xl antialiased tracking-wide text-justify">{HtmlParser(data.blog.content)}</div>
