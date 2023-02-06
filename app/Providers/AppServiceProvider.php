22fd7e32520f6d00c0f2d359b48f1e0a38a2e0bd
Juddan <juddan2008@gmail.com>
2023-02-06 23:05:09 +0530

    contact

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,6 @@ namespace App\Providers;
 use Filament\Facades\Filament;
 use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
-use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment(['staging', 'prod'])) {
-            URL::forceScheme('https');
-        }
         Filament::serving(function() {
             Filament::registerTheme(
                 app(Vite::class)('resources/css/filament.css')
