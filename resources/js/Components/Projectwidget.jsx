5c37fa418e2a6bc71964f08718dc3fbda1c2d8b3
Juddan <juddan2008@gmail.com>
2023-02-08 23:40:03 +0530

    img

--- resources/js/Components/Projectwidget.jsx
+++ resources/js/Components/Projectwidget.jsx
@@ -55,7 +55,7 @@ const ProjectWidget = () => {
                                     key={project.id}
                                     src={ pathUrl + project.gallery[0]}
                                     alt={project.title}
-                                    className="h-auto w-auto object-cover rounded-t-sm"
+                                    className="h-64 w-auto object-cover rounded-t-sm"
                                 /></div>
                                 <div className="p-4">
                                     
